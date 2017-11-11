278f0a9f1b64ef8e97c44a41826ad56e85a35760
timothehippo <32986416+timothehippo@users.noreply.github.com>
2017-11-11 13:25:27 -0500

    Update BlitzBotProtocol.java

--- BlitzBotProtocol.java
+++ BlitzBotProtocol.java
@@ -9,7 +9,13 @@ import hlt.*;
 public class BlitzBotProtocol {
 
 	public void blitz(final GameMap game, ArrayList<Move> MoveList) {
-		
+		Player enemyplayer = null;
+		for(Player player : game.getAllPlayers().values){
+			if(player.equals(game.getMyPlayer())){
+			enemyplayer = player;
+				break;
+			}
+		}
 		for(Ship ship : game.getMyPlayer().getShips().values()) {
 			for(Ship enemyship : enemyplayer.getShips().values()) {
 				if(enemyship.getDockingStatus()==Ship.DockingStatus.Undocked) {
