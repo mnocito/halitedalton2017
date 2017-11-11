0af078059f17fe807546d88212d29201a6bd197e
timothehippo <32986416+timothehippo@users.noreply.github.com>
2017-11-11 13:26:39 -0500

    Update BlitzBotProtocol.java

--- BlitzBotProtocol.java
+++ BlitzBotProtocol.java
@@ -10,7 +10,7 @@ public class BlitzBotProtocol {
 
 	public void blitz(final GameMap game, ArrayList<Move> MoveList) {
 		Player enemyplayer = null;
-		for(Player player : game.getAllPlayers().values){
+		for(Player player : game.getAllPlayers()){
 			if(player.equals(game.getMyPlayer())){
 			enemyplayer = player;
 				break;
