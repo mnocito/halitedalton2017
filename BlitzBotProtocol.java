a81c67c6b95027844aa6c93b667ed25f711a8f57
timothehippo <32986416+timothehippo@users.noreply.github.com>
2017-11-11 13:54:22 -0500

    Update BlitzBotProtocol.java

--- BlitzBotProtocol.java
+++ BlitzBotProtocol.java
@@ -16,85 +16,29 @@ public class BlitzBotProtocol {
 				break;
 			}
 		}
-		for(Ship ship : game.getMyPlayer().getShips().values()) {
-			for(Ship enemyship : enemyplayer.getShips().values()) {
-				if(enemyship.getDockingStatus()==Ship.DockingStatus.Undocked) {
-					for (final Planet planet : game.getAllPlanets().values()) {
-
-
-
-					if (planet.isOwned()) {
-						continue;
-					}
-
-
-					if (ship.canDock(planet)) {
-						MoveList.add(new DockMove(ship, planet));
-						break;
-					}
-
-					final ThrustMove thrust = Navigation.navigateShipToDock(game, ship, planet, Constants.MAX_SPEED);
-					if (thrust != null) {
-						MoveList.add(thrust);
-
-						break;
-					}
-
-
-
-
-				}
-				break;
-				}
-				
-				final ThrustMove newThrustMove = Navigation.navigateShipTowardsTarget(game, ship, ship.getClosestPoint(enemyship), Constants.MAX_SPEED, true, Constants.MAX_NAVIGATION_CORRECTIONS, Math.PI/180.0);
-			
-				if(newThrustMove!=null) {
+		for(Ship enemyship : enemyplayer.getShips().values()) {
+				if(enemyship.getDockingStatus()==Ship.DockingStatus.Docked) {
+					for(Ship ship : game.getMyPlayer().getShips().values()) {
+						final ThrustMove newThrustMove = Navigation.navigateShipTowardsTarget(game, ship, ship.getClosestPoint(enemyship), Constants.MAX_SPEED, true, Constants.MAX_NAVIGATION_CORRECTIONS, Math.PI/180.0);
 					MoveList.add(newThrustMove);
-					break;
+					}
+					return;
 				}
-				/*
-					for (final Planet planet : game.getAllPlanets().values()) {
-		                  
-
-		                 
-		                 if (planet.isOwned()) {
-		                     continue;
-		                 }
-		                
-
-		                 if (ship.canDock(planet)) {
-		                     MoveList.add(new DockMove(ship, planet));
-		                     break;
-		                 }
-
-		                 final ThrustMove thrust = Navigation.navigateShipToDock(game, ship, planet, Constants.MAX_SPEED);
-		                 if (thrust != null) {
-		                     MoveList.add(thrust);
-		                     
-		                     break;
-		                 }
-		                 
-		                 
-		             
-					
-				}
-				*/
 			}
-
-
-		}
-		
-		
-
-return;
-	
-
-
-
-//return list of move orders
-
-
-}
+			for(Ship enemyship : enemyplayer.getShips().values()) {
+				
+				if(enemyship!=null) {
+					for(Ship ship : game.getMyPlayer().getShips().values()) {
+						final ThrustMove newThrustMove = Navigation.navigateShipTowardsTarget(game, ship, ship.getClosestPoint(enemyship), Constants.MAX_SPEED, true, Constants.MAX_NAVIGATION_CORRECTIONS, Math.PI/180.0);
+					MoveList.add(newThrustMove);
+					}
+					return;
+				}
+					
+					
+				
+			}
 	
 }
+	
+	
